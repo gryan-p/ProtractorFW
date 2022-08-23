8fdddaaa917bd3410dbd2d0ac1fd8a7de15e12fc
Gryan Panulaya <GPanulaya@Arcanys.com>
2022-08-23 13:12:35 +0800

    update calculator for Jenkins

--- test/calculator.js
+++ test/calculator.js
@@ -3,22 +3,13 @@ describe('demo calculator tests', function () {
 
     it('addition test', function () {
         homepage.get('http://juliemr.github.io/protractor-demo/');
-        homepage.enterFirstnumber('1');
+        homepage.enterFirstnumber('12');
         homepage.enterSecondnumber('3');
         homepage.clickGo();
-        homepage.verifyResult('4');
-        browser.sleep(3000)
-    });
-
-    it('addition test', function () {
-        homepage.get('http://juliemr.github.io/protractor-demo/');
-        homepage.enterFirstnumber('1');
-        homepage.enterSecondnumber('3');
-        homepage.clickGo1();
-        homepage.verifyResult('4');
+        homepage.verifyResult('5');
         browser.sleep(3000)
     });
     //it('subtractiontes', function () {
 
-    //})
+    //}) 
 });
