187e65f3111332bdd0013b8d7241eaa6f569f255
Gryan Panulaya <GPanulaya@Arcanys.com>
2022-08-23 13:14:57 +0800

    update calculator for Jenkins again

--- test/calculator.js
+++ test/calculator.js
@@ -3,7 +3,7 @@ describe('demo calculator tests', function () {
 
     it('addition test', function () {
         homepage.get('http://juliemr.github.io/protractor-demo/');
-        homepage.enterFirstnumber('12');
+        homepage.enterFirstnumber('2');
         homepage.enterSecondnumber('3');
         homepage.clickGo();
         homepage.verifyResult('5');
